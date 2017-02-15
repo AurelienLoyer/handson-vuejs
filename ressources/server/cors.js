4d121a4c60db1d059c5195a2435b899b7bbf685d
Loyer Aurélien <aurelien.loyer@zenika.com>
2017-02-15 13:49:29 +0100

    Node Rest api http://localhost:8080/api/v1/beers

---
+++ ressources/server/cors.js
@@ -0,0 +1,25 @@
+module.exports = function (req, res, next) {
+  var oneof = false;
+  if (req.headers.origin) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    oneof = true;
+  }
+  if (req.headers['access-control-request-method']) {
+    res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+    oneof = true;
+  }
+  if (req.headers['access-control-request-headers']) {
+    res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+    oneof = true;
+  }
+  if (oneof) {
+    res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+  }
+
+  // intercept OPTIONS method
+  if (oneof && req.method == 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+}
