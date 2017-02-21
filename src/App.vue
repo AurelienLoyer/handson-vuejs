640eb2846aa02e93b5fdd88c67069f1a97ecb65b
Loyer Aurélien <aurelien.loyer@zenika.com>
2017-02-21 13:44:57 +0100

    Remove router and add home component to App

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <v-home></v-home>
   </div>
 </template>
 
 <script>
+
+import Home from 'components/Home'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    'v-home': Home
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
