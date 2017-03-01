23c3b0bb1030756859699341ec48cacec2954d88
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2017-03-01 14:57:09 +0100

    Solve filters

--- src/main.js
+++ src/main.js
@@ -2,24 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-// import Filters from './filters/capitalize.js'
-// var Filters = require('./filters/index.js');
-
-Vue.filter('capitalize', function (value) {
-  if (!value) return ''
-  value = value.toString()
-  return value.charAt(0).toUpperCase() + value.slice(1)
-})
-
-Vue.filter('uppercase', function (value) {
-  if (!value) return ''
-  value = value.toString()
-  return value.toUpperCase()
-})
+import './filters/index'
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  template: '<App/>',
+  template: '<App />',
   components: { App }
 })
