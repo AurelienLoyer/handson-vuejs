3ac1068080e60b4277944e2c6983d07517d88a83
Aurélien Loyer <aur.loy@gmail.com>
2019-04-07 22:04:39 -0400

    chore: update to vuecli 3 ✌️

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import './filters/index'
 
-/* eslint-disable no-new */
+Vue.config.productionTip = false
+
 new Vue({
-  el: '#app',
-  template: '<App />',
-  components: { App }
-})
+  render: h => h(App),
+}).$mount('#app')
